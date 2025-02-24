c8004a2cc7e7f74afdabebb6a8527799f5716504
Rishabh Chouhan <123556976+Rishabh28n@users.noreply.github.com>
2025-02-25 02:16:16 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+const parent = React.createElement("div",{id:"parent"},
+
+   [ React.createElement("div",
+    {id:"child1"},
+    [React.createElement("h1",{},"I'm an h1 tag"), React.createElement("h2",{},"I am an h2 tag"),React.createElement("h3",{},"I am in h3 tag")]
+  ), React.createElement("div",
+    {id:"child2"},
+    [React.createElement("h1",{},"I'm an h1 tag"), React.createElement("h2",{},"I am an h2 tag"),React.createElement("h3",{},"I am in h3 tag")]
+  )]
+);
+console.log(parent)
+
+
+
+
+
+
+
+
+const heading = React.createElement("h1",{id:"heading"},"Hello world from react! ")
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+   root.render(parent);
